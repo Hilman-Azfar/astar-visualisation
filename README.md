203ba8796a99cdab9e7d582f33b7368cde696d8c
Hilman-Azfar <m.hilman.azfar@gmail.com>
2020-11-15 17:09:29 +0800

    random start end

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ## motivations
 
 I have always been intrigued by a robots ability to navigate its surroundings. A popular algorithm used to find the shortest path is the A* search algorithm. In this project, I want to learn and visualise the steps needed to achieve this.
+
+references:
+https://medium.com/wdstack/fixing-html5-2d-canvas-blur-8ebe27db07da
+https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_PathFinding_AStar.cpp
+https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 console.log("Starting");
 
-function setUpGrid(gridWidth, gridHeight) {
-    // canvas is made up of a 2d array with each position being a node.
-    class Node {
-        constructor(x, y) {
-            this.isObstacle = false;
-            this.visited = false;
-            this.GlobalGoal = null;
-            this.LocalGoal = null;
-            this.x = x;
-            this.y = y;
-            this.neighbours = [];
-            this.parent = null;
-        }
+let myGrid = {
+    area: function () { return parseInt(document.getElementById("area").value) },
+    grid: null,
+    start: null,
+    end: null,
+}
+
+class Node {
+    constructor(x, y) {
+        this.name = null;
+        this.x = x;
+        this.y = y;
+        this.isObstacle = false;
+        this.visited = false;
+        this.GlobalGoal = null;
+        this.LocalGoal = null;
+        this.neighbours = [];
+        this.parent = null;
     }
+}
+
+function setUpGrid() {
+    // canvas is made up of a 2d array with each position being a node.
+
     // create a grid to hold the nodes
     // grid shape in an array
     // [
@@ -22,17 +32,20 @@ function setUpGrid(gridWidth, gridHeight) {
     //     [x, x, x]
     // ]
 
-    let grid = new Array(gridWidth).fill(new Array(gridHeight));
+    const gridHeight = myGrid.area();
+    const gridWidth = myGrid.area();
+    let grid = Array.from(Array(gridWidth), () => new Array(gridHeight));
 
     // populate with nodes
     for (let x = 0; x < gridWidth; x++) {
         for (let y = 0; y < gridHeight; y++) {
             let newNode = new Node(x, y);
+            newNode.name = `${x} and ${y}`;
             grid[x][y] = newNode;
         }
     }
 
-    // create connections to every neighbouring nodes
+    //create connections to every neighbouring nodes
     for (let x = 0; x < gridWidth; x++) {
         for (let y = 0; y < gridHeight; y++) {
             // if not at the left end, add the previous node to the left
@@ -53,47 +66,72 @@ function setUpGrid(gridWidth, gridHeight) {
             }
         }
     }
-    return grid;
+    myGrid.grid = grid;
 }
 
 let dpi = window.devicePixelRatio;
 const canvas = document.getElementById("display");
 const context = canvas.getContext("2d");
 
-context.fillStyle = 'lightgrey';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 function fixDpi() {
     let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
     let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
     canvas.setAttribute('height', style_height * dpi);
     canvas.setAttribute('width', style_width * dpi);
-    return style_height * dpi;
 }
 
 document.getElementById("setParams").onsubmit = function (e) {
     e.preventDefault();
-    // get new area
-    // get set new sizing
-    // redraw
-    let side = fixDpi();
+    setUpGrid();
+    setRandomStartEnd();
+    draw();
+};
 
-    let area = parseInt(e.target[0].value); // no of grid area
-    let gridSize = side / area;
-    let padding = 1;
+window.onload = function () {
+    setUpGrid();
+    setRandomStartEnd();
+    draw();
+}
 
-    let newGrid = setUpGrid(area, area);
+function draw() {
+    fixDpi();
+
+    const area = myGrid.area();
+    let gridSize = canvas.width / area;
 
     context.clearRect(0, 0, canvas.width, canvas.height);
-
+    const padding = 3;
     for (let x = 0; x < area; x++) {
         for (let y = 0; y < area; y++) {
             // get color
-            context.fillStyle = newGrid[x][y].isObstacle ? 'grey' : 'blue';
+            let color = 'blue';
+            if (myGrid.grid[x][y].isObstacle) color = 'grey';
+            if (myGrid.start === myGrid.grid[x][y]) color = 'green';
+            if (myGrid.end === myGrid.grid[x][y]) color = 'red';
+            context.fillStyle = color;
             context.fillRect(x * gridSize + padding, y * gridSize + padding, gridSize - padding * 2, gridSize - padding * 2);
         }
     }
-};
+}
+
+function setRandomStartEnd() {
+    const area = myGrid.area();
+    let c = getRandomXY(area);
+    myGrid.start = myGrid.grid[c.x][c.y];
+    let t;
+    do {
+        t = getRandomXY(area);
+    } while (t === c)
+    myGrid.end = myGrid.grid[t.x][t.y];
+}
+
+function getRandomXY(area) {
+
+    return {
+        x: Math.floor(Math.random() * (area)),
+        y: Math.floor(Math.random() * (area))
+    }// inclusive
+}
 
 
 
